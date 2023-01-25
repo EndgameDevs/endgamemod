6211599c34116ac2f76a2bb915a28078f72f5db8
DreamingCodes <dev@dreaming.codes>
2023-01-25 13:41:46 +0100

    Added AbstractGeckoArmorItem for creating animated Gecko armatures

---
+++ common/src/main/java/dev/endgame/items/AbstractGeckoArmorItem.java
@@ -0,0 +1,58 @@
+package dev.endgame.items;
+
+import dev.architectury.injectables.annotations.PlatformOnly;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import software.bernie.geckolib.animatable.GeoItem;
+import software.bernie.geckolib.animatable.SingletonGeoAnimatable;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.renderer.GeoArmorRenderer;
+import software.bernie.geckolib.util.GeckoLibUtil;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public abstract class AbstractGeckoArmorItem extends ArmorItem implements GeoItem {
+
+    protected final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
+
+    protected HumanoidModel<LivingEntity> renderer;
+
+    public AbstractGeckoArmorItem(ArmorMaterial armorMaterial, EquipmentSlot slot, Properties properties) {
+        super(armorMaterial, slot, properties);
+    }
+
+    protected abstract GeoArmorRenderer<?> getRenderer();
+
+    protected void registerSyncedAnimatable() {
+        SingletonGeoAnimatable.registerSyncedAnimatable(this);
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return cache;
+    }
+
+
+    @Override
+    @PlatformOnly("fabric")
+    public void createRenderer(Consumer<Object> consumer) {
+        throw new AssertionError("This method should be overridden by the platform.");
+    }
+
+    @PlatformOnly("forge")
+    @SuppressWarnings("unused")
+    public void initializeClient(Consumer<Object> consumer) {
+        throw new AssertionError("This method should be overridden by the platform.");
+    }
+
+    @Override
+    @PlatformOnly("fabric")
+    public Supplier<Object> getRenderProvider() {
+        throw new AssertionError("This method should be overridden by the platform.");
+    }
+
+}
---
+++ forge/src/main/java/dev/endgame/forge/mixin/AbstractForgeGeckoArmorItem.java
@@ -0,0 +1,47 @@
+package dev.endgame.forge.mixin;
+
+import dev.endgame.items.AbstractGeckoArmorItem;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import software.bernie.geckolib.animatable.GeoItem;
+import software.bernie.geckolib.renderer.GeoArmorRenderer;
+
+import java.util.function.Consumer;
+
+@Mixin(AbstractGeckoArmorItem.class)
+public abstract class AbstractForgeGeckoArmorItem extends ArmorItem implements GeoItem {
+    public AbstractForgeGeckoArmorItem(ArmorMaterial armorMaterial, EquipmentSlot equipmentSlot, Properties properties) {
+        super(armorMaterial, equipmentSlot, properties);
+    }
+
+    @Shadow(remap = false)
+    protected abstract GeoArmorRenderer<?> getRenderer();
+
+    /**
+     * @author raccoman
+     * @reason createRendered is needed only for fabric
+     */
+    @Overwrite(remap = false)
+    public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+        consumer.accept(new IClientItemExtensions() {
+            @Override
+            public @NotNull HumanoidModel<?> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, HumanoidModel<?> original) {
+                // This prepares our GeoArmorRenderer for the current render frame.
+                // These parameters may be null however, so we don't do anything further with them
+                getRenderer().prepForRender(livingEntity, itemStack, equipmentSlot, original);
+
+                return getRenderer();
+            }
+        });
+    }
+
+}
